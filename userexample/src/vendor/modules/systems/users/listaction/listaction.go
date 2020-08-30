0b56b7e433ee6aec6e5b3e0958ccb151afdb30d0
jarlyyn <jarlyyn@qq.com>
2020-08-30 22:24:12 +0800

    update

--- userexample/src/vendor/modules/systems/users/listaction/listaction.go
+++ userexample/src/vendor/modules/systems/users/listaction/listaction.go
@@ -23,7 +23,7 @@ type Result struct {
 }
 
 var ActionList = action.New(func(w http.ResponseWriter, r *http.Request) {
-	users, _, err := members.Status.Service.ListUsersByStatus("", 0, status.StatusBanned, status.StatusNormal)
+	users, err := members.Status.Service.ListUsersByStatus("", 0, status.StatusBanned, status.StatusNormal)
 	if err != nil {
 		panic(err)
 	}
